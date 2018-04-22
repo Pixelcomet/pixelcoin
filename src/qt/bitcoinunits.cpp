e164ad73be2cfb11c8c61c950a8fbb210052a13e
Erik Neumann <erikneumannmw@googlemail.com>
2018-04-22 14:16:39 +0200

    Renamed 'lites' to 'pixels'

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -41,7 +41,7 @@ QString BitcoinUnits::name(int unit)
     switch(unit)
     {
     case BTC: return QString("PXC");
-    case mBTC: return QString("lites");
+    case mBTC: return QString("pixels");
     case uBTC: return QString("photons");
     default: return QString("???");
     }
@@ -52,7 +52,7 @@ QString BitcoinUnits::description(int unit)
     switch(unit)
     {
     case BTC: return QString("Pixelcoins");
-    case mBTC: return QString("Lites (1 / 1" THIN_SP_UTF8 "000)");
+    case mBTC: return QString("Pixels (1 / 1" THIN_SP_UTF8 "000)");
     case uBTC: return QString("Photons (1 / 1" THIN_SP_UTF8 "000" THIN_SP_UTF8 "000)");
     default: return QString("???");
     }
